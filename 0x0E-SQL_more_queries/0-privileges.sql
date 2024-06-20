1395c00e843c0b903290798f574116d0ac5e76fe
Haile1221 <hzerfu10@gmail.com>
2024-06-20 02:28:52 -0700

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,12 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
+-- Create the users if they don't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant all privileges to the users
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- List the privileges of the users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
