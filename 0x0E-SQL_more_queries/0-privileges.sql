cbe1a4bf4c1be3940c5cdc0dd6cdd17d0291759b
Haile1221 <hzerfu10@gmail.com>
2024-06-20 03:04:31 -0700

    mysql updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,4 @@
--- Create the users if they don't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to the users
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- List the privileges of the users
+-- List the privileges of the users if they exist
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
