84d32f4474d36f37bc16e9325aea151b9eb19388
Haile1221 <hzerfu10@gmail.com>
2024-08-22 23:15:11 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const file = process.argv[2];
+const fs = require('fs'); // Import the 'fs' module
+const file = process.argv[2]; // Get the file path from the command line argument
 
+// Read the file with utf-8 encoding
 fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
+    // If there's an error, print the error object
     console.log(err);
   } else {
+    // Otherwise, print the content of the file
     console.log(data);
   }
 });
